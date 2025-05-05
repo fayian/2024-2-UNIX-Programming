70cc6e3f1ee90f7711dcdecf91f845d4fb31d686
fayian <iamfarmer1415@gmail.com>
2025-05-05 12:58:38 +0800

    Finished Lab4

--- Lab4/libmini64-ext.asm
+++ Lab4/libmini64-ext.asm
@@ -1,7 +1,25 @@
+%macro gensys 2
+	global sys_%2:function
+sys_%2:
+	push	r10
+	mov	r10, rcx
+	mov	rax, %1
+	syscall
+	pop	r10
+	ret
+%endmacro
+
+extern write_hex
+
 section .data
     seed dq 0
 
 section .text
+
+gensys 13, rt_sigaction
+gensys 14, rt_sigprocmask
+gensys 127, rt_sigpending
+gensys 130, rt_sigsuspend
 
 global time:function
 time:
@@ -38,7 +56,8 @@ sigemptyset:
 
 global sigfillset:function
 sigfillset:
-    mov QWORD[rdi], 0xffffffffffffffff
+    mov rdx, 0xffffffffffffffff
+    mov [rdi], rdx
     ret
 
 global sigaddset:function
@@ -56,7 +75,7 @@ sigdelset:
     mov rcx, rsi
     sub rcx, 1
     shl edx, cl
-    xor rdx, 0xffffffffffffffff
+    not rdx
     and [rdi], rdx
     ret
 
@@ -75,3 +94,63 @@ sigismember:
 sigismember_isset:
     mov rax, 1
     ret
+
+global sigprocmask:function
+sigprocmask:
+    mov rcx, 8
+    call sys_rt_sigprocmask
+    ret
+
+global setjmp:function
+setjmp:
+    mov [rdi], rbx
+    mov [rdi + 8], r12
+    mov [rdi + 16], r13
+    mov [rdi + 24], r14
+    mov [rdi + 32], r15
+    mov [rdi + 40], rsp
+    mov [rdi + 48], rbp
+    mov rcx, [rsp] ; return address
+    mov [rdi + 56], rcx
+
+    push rdi
+    sub rsp, 8
+    mov rsi, 0
+    mov rdx, rsp
+    call sigprocmask
+    mov rcx, [rsp]
+    add rsp, 8
+    pop rdi
+    
+    mov [rdi + 64], rcx
+    mov rax, 0
+    ret
+
+global longjmp:function
+longjmp:
+    mov rcx, [rdi + 64]
+
+    push rdi
+    push rsi
+    sub rsp, 8
+    mov [rsp], rcx
+    mov rdi, 2
+    mov rsi, rsp
+    mov rdx, 0
+    call sigprocmask
+    add rsp, 8
+    pop rsi
+    pop rdi
+
+    mov rbx, [rdi]
+    mov r12, [rdi + 8]
+    mov r13, [rdi + 16]
+    mov r14, [rdi + 24]
+    mov r15, [rdi + 32]
+    mov rsp, [rdi + 40]
+    mov rbp, [rdi + 48]    
+
+    mov rax, rsi
+    pop rcx
+    mov rcx, [rdi + 56]
+    jmp rcx
